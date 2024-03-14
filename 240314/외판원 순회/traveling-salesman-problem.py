2553c1466ca773d1536a1841d4a524c8998287e5
BONG-KI <69136398+JB0527@users.noreply.github.com>
2024-03-14 15:01:03 +0900

    Title: 외판원 순회, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240314/외판원 순회/traveling-salesman-problem.py
@@ -0,0 +1,34 @@
+import itertools
+"""n = int(input())
+info = [list(map(int,input().split())) for _ in range(n)]
+chars = []
+for i in range(n):
+    chars.append(i)
+order = itertools.permutations(chars, 2)
+answer = 0
+
+for i,j in order:
+    if not info[i][j] == 0:
+        answer += info[i][j]
+print(answer)
+"""
+import itertools
+#외판원순열에 1로돌아가는거까지 추가
+def calculate_cost(path, info):
+    cost = 0
+    n = len(path)
+    for i in range(n - 1):
+        cost += info[path[i]][path[i + 1]]
+    cost += info[path[-1]][path[0]]  
+    return cost
+
+n = int(input())
+info = [list(map(int, input().split())) for _ in range(n)]
+chars = list(range(n))
+min_cost = float('inf')
+
+for path in itertools.permutations(chars):
+    cost = calculate_cost(path, info)
+    min_cost = min(min_cost, cost)
+
+print(min_cost)
