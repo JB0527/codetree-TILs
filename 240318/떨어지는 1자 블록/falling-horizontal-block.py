ef8745100baa95cc07e56968e0048f33aca20f68
BONG-KI <69136398+JB0527@users.noreply.github.com>
2024-03-18 14:18:32 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240318/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240318/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,18 +1,18 @@
 n, m ,k = map(int,input().split())
 arr = [list(map(int,input().split())) for _ in range(n)]
 
-for j in range(n-1):
-    for i in range(m-1):
-        if arr[j][k+i-1] == 0:
+for j in range(n):
+    for i in range(m):
+        if arr[j-1][k+i-1] == 0:
             True
-        elif arr[j][k+i-1] == 1:
-            tmp = j-1
+        elif arr[j-1][k+i-1] == 1:
+            tmp = j-2
             break
         else:
             tmp = n-1
             
-for i in range(k-1, k+m-1):
-    arr[tmp][i] = 1
+for i in range(k, k+m):
+    arr[tmp][i-1] = 1
 
 for i in range(n):
     for j in range(n):
