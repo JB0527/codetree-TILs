f73769227cfe2644a8ba921411b3979c8e845ccb
BONG-KI <69136398+JB0527@users.noreply.github.com>
2024-03-18 14:08:00 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240318/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,19 @@
+n, m ,k = map(int,input().split())
+arr = [list(map(int,input().split())) for _ in range(n)]
+
+for j in range(n-1):
+    for i in range(m-1):
+        if arr[j][k+i-1] == 0:
+            True
+        else:
+            arr[j-1][k+i-1] = 1
+            tmp = j-1
+            break
+
+for i in range(k-1, k+m-1):
+    arr[tmp][i] = 1
+
+for i in range(n):
+    for j in range(n):
+        print(arr[i][j], end = " ")
+    print()
