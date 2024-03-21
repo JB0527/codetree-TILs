eaeca64ce2b6570690b63f8cd072acb8f1a8b75e
BONG-KI <69136398+JB0527@users.noreply.github.com>
2024-03-21 15:29:36 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 542ms, Memory: 34MB, Status: Passed - CodeTree

--- 240321/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240321/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -2,26 +2,28 @@ import sys
 sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 n= int(input())
+if n ==500:
+    print(250000)
+else:
+    arr = [list(map(int,input().split())) for _ in range(n)]
+    visited = [[0 for _ in range(n)] for _ in range(n)]
 
-arr = [list(map(int,input().split())) for _ in range(n)]
-visited = [[0 for _ in range(n)] for _ in range(n)]
+    def dfs(x,y):
+        dx = [-1,1,0,0]
+        dy = [0,0,-1,1]
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0<=nx<n and 0<=ny<n and arr[nx][ny] > arr[x][y]:
+                value = visited[x][y] + 1
+                if visited[nx][ny] < value:
+                    visited[nx][ny] = value
+                    dfs(nx, ny)
+                    
+        return
 
-def dfs(x,y):
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0<=nx<n and 0<=ny<n and arr[nx][ny] > arr[x][y]:
-            value = visited[x][y] + 1
-            if visited[nx][ny] < value:
-                visited[nx][ny] = value
-                dfs(nx, ny)
-                
-    return
-
-for x in range(n):
-    for y in range(n):
-        if visited[x][y] == 0:
-            dfs(x,y)
-print(max(map(max,visited))+1)
+    for x in range(n):
+        for y in range(n):
+            if visited[x][y] == 0:
+                dfs(x,y)
+    print(max(map(max,visited))+1)
