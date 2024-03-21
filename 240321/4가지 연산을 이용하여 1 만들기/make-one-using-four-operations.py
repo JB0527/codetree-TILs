7bd9d60ba430c6a122b15bca7002544cf96a15b1
BONG-KI <69136398+JB0527@users.noreply.github.com>
2024-03-21 15:05:59 +0900

    Title: 4가지 연산을 이용하여 1 만들기, Time: 186ms, Memory: 25MB, Status: Passed - CodeTree

--- 240321/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
+++ 240321/4가지 연산을 이용하여 1 만들기/make-one-using-four-operations.py
@@ -1,3 +1,4 @@
+"""
 import sys
 from collections import deque
 input = sys.stdin.readline
@@ -16,17 +17,59 @@ def bfs():
             answer = time
             return print(answer)
 
-        elif num//3 not in visited and num % 3 == 0:
+        elif num // 3 not in visited and num % 3 == 0:
             visited.append(num // 3)
             que.append((num//3,time + 1))
-        elif num//2 not in visited and num % 2 == 0:
+                
+        elif num // 2 not in visited and num % 2 == 0:
             visited.append(num // 2)
             que.append((num//2,time + 1))
-        elif num-1 not in visited:
-            visited.append(num-1)
-            que.append((num-1,time + 1))
-        elif num+1 not in visited:
-            visited.append(num+1)
-            que.append((num+1,time + 1)) 
-            
+                
+        elif num % 2 != 0 and num% 3 != 0:
+            if num-1 not in visited and (num-1) % 3 == 0:
+                visited.append(num-1)
+                que.append((num-1,time + 1)) 
+            elif num+1 not in visited and (num+1) % 3 == 0:
+                visited.append(num+1)
+                que.append((num+1,time + 1))
+            elif num-1 not in visited and (num-2) % 3 == 0:
+                visited.append(num-1)
+                que.append((num-1,time + 1)) 
+            elif num-1 not in visited and (num-1) % 2 == 0:
+                visited.append(num-1)
+                que.append((num-1,time + 1))
 bfs()
+"""
+import sys
+input = sys.stdin.readline 
+from collections import deque
+
+n = int(input())
+que = deque()
+que.append((n, 0))
+visited = [n]
+answer = 0
+
+def bfs():
+    global answer
+    while que:
+        now_num, time= que.popleft()
+        if now_num == 1:
+            answer = time
+            return 
+
+        if now_num//3 not in visited and now_num % 3 == 0:
+            visited.append(now_num//3)
+            que.append((now_num//3, time+1))
+        if now_num//2 not in visited and now_num % 2 == 0:
+            visited.append(now_num//2)
+            que.append((now_num//2, time+1))
+        if now_num-1 not in visited:
+            visited.append(now_num-1)
+            que.append((now_num-1, time+1))
+        if now_num+1 not in visited:
+            visited.append(now_num+1)
+            que.append((now_num+1, time+1))
+
+bfs()
+print(answer)
