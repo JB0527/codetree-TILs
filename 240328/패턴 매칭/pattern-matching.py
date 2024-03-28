7d78bb8cda63576392730df1ae72bc78c5743d54
BONG-KI <69136398+JB0527@users.noreply.github.com>
2024-03-28 14:54:57 +0900

    Title: 패턴 매칭, Time: 118ms, Memory: 24MB, Status: Passed - CodeTree

--- 240328/패턴 매칭/pattern-matching.py
+++ 240328/패턴 매칭/pattern-matching.py
@@ -1,3 +1,4 @@
+'''
 s = str(input())
 p = str(input())
 
@@ -16,7 +17,7 @@ for j in s:
             real = l
         else:
             real = 0
-
+answer = []
 for i, tmp in enumerate(p[real:]):
     for j in s:
         if j != tmp:
@@ -26,7 +27,7 @@ for i, tmp in enumerate(p[real:]):
             elif tmp == '.':
                 continue
             else:
-                answer = 'false'
+                answer.append(j)
                 break
         elif j == tmp:
             continue
@@ -36,3 +37,33 @@ for i, tmp in enumerate(p[real:]):
     else:
         print('true')
         break
+'''
+s = input()
+p = input()
+
+len_s = len(s)
+len_p = len(p)
+
+# Initialize a 2D DP table
+dp = [[False] * (len_p + 1) for _ in range(len_s + 1)]
+
+# Base case: empty string and empty pattern match
+dp[0][0] = True
+
+# Handle cases where '*' in the pattern can match an empty sequence
+for j in range(2, len_p + 1):
+    if p[j - 1] == '*':
+        dp[0][j] = dp[0][j - 2]
+
+for i in range(1, len_s + 1):
+    for j in range(1, len_p + 1):
+        if p[j - 1] == '.' or p[j - 1] == s[i - 1]:
+            dp[i][j] = dp[i - 1][j - 1]
+        elif p[j - 1] == '*':
+            dp[i][j] = dp[i][j - 2] or (dp[i - 1][j] and (s[i - 1] == p[j - 2] or p[j - 2] == '.'))
+
+# Check if the last cell of the DP table is True
+if dp[len_s][len_p]:
+    print('true')
+else:
+    print('false')
