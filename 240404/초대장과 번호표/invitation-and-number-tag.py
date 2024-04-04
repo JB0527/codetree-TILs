5c04d39b91fb08c55e4c0a337a2e573d57dddd1b
BONG-KI <69136398+JB0527@users.noreply.github.com>
2024-04-04 15:06:09 +0900

    Title: 초대장과 번호표, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240404/초대장과 번호표/invitation-and-number-tag.py
+++ 240404/초대장과 번호표/invitation-and-number-tag.py
@@ -2,7 +2,8 @@ N, G = map(int, input().split())
 
 arr = [list(map(int, input().split())) for _ in range(G)]
 s = set() #이미 초대장을 받은 친구들
-ans=[1]
+s.add(1)
+ans = []
 for i in range(G):
     num = arr[i][0]
     stmp = set(arr[i][1:])
@@ -13,5 +14,18 @@ for i in range(G):
     if num <= 2 :
         for p in stmp:
             ans.append(p)
+    arr[i][0] = len(stmp)
+    arr[i][1:] = list(stmp)
 #print(ans)
+
+for i in range(G):
+    num = arr[i][0]
+    stmp = set(arr[i][1:])
+    for k in ans:
+        if k in stmp:
+            stmp.remove(k)
+            num -= 1
+    if num <= 2 :
+        for p in stmp:
+            ans.append(p)
 print(len(set(ans)))
