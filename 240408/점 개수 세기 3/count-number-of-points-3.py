773b6ef95b128f9588d8c68aa938729d9390ac9e
BONG-KI <69136398+JB0527@users.noreply.github.com>
2024-04-08 10:42:07 +0900

    Title: 점 개수 세기 3, Time: 5098ms, Memory: 48MB, Status: Time Limit Exceed - CodeTree

--- 240408/점 개수 세기 3/count-number-of-points-3.py
+++ 240408/점 개수 세기 3/count-number-of-points-3.py
@@ -3,17 +3,17 @@ from sortedcontainers import SortedSet
 n,q = map(int,input().split())
 pos = list(map(int,input().split()))
 edges = [list(map(int,input().split())) for _ in range(q)]
-nums = SortedSet()
+#nums = SortedSet()
 
 # 사용되는 모든 번호를 treeset에 넣어줍니다.
-for v1 in pos:
-    nums.add(v1)
+#for v1 in pos:
+#    nums.add(v1)
 
 # treeset에서 정점을 작은 번호부터 뽑으면서
 # 각 정점별로 1번부터 순서대로 매칭하여
 # 그 결과를 hashmap에 넣어줍니다.
-start = nums[0]
-end = nums[-1]
+start = pos[0]
+end = pos[-1]
 mapper = dict()
 cnt = 1
 for i in range(start,end+1):
