e42a1ff846c7c0238d3be4fe98cae3e9705c23a2
BONG-KI <69136398+JB0527@users.noreply.github.com>
2024-05-30 14:55:09 +0900

    Title: 숫자들의 합이 7의 배수, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240530/숫자들의 합이 7의 배수/the-sum-of-the-numbers-is-a-multiple-of-7.py
@@ -0,0 +1,22 @@
+n = int(input())
+arr = [int(input()) for i in range(n)]
+
+tmp = 0
+for i,value in enumerate(arr):
+    if  value % 7 == 0:
+        tmp += 1
+        arr.pop(i)
+
+def finding(arr):
+    answer = 0
+    find = arr.pop()
+    for j,value in enumerate(arr):
+        if (find + j) % 7 == 0:
+            answer += 2
+            arr.pop(j)
+    return answer
+answers = 0
+while len(arr) > 1:
+    answers += finding(arr)
+#print(arr)
+print(answers+tmp)
