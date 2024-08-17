7677fe95e8b93d972d3a129d2158c0015a6a22bb
BONG-KI <69136398+JB0527@users.noreply.github.com>
2024-08-17 23:30:29 +0900

    Title: 문자열 만들기, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240817/문자열 만들기/create-string.py
+++ 240817/문자열 만들기/create-string.py
@@ -1,20 +1,31 @@
+def make_lexicographically_smallest_string(n, S):
+    left, right = 0, n - 1
+    T = []
+    
+    while left <= right:
+        if S[left] < S[right]:
+            T.append(S[left])
+            left += 1
+        elif S[left] > S[right]:
+            T.append(S[right])
+            right -= 1
+        else:
+            l, r = left, right
+            while l < r and S[l] == S[r]:
+                l += 1
+                r -= 1
+            if S[l] < S[r]:
+                T.append(S[left])
+                left += 1
+            else:
+                T.append(S[right])
+                right -= 1
+    
+    return ''.join(T)
+
+# 입력
 n = int(input())
-s = list(str(input()))
-T = []
-def check():
-    if s[0] < s[-1]:
-        T.append(s[0])
-        s.remove(s[0])
-        #print(s)
-    else:
-        T.append(s[-1])
-        s.pop()
-        #print(s)
+S = input()
 
-
-for i in range(n):
-    if s[0] != s[-1]:
-        check()
-    else:
-        check()
-print("".join(T))
+# 결과 출력
+print(make_lexicographically_smallest_string(n, S))
