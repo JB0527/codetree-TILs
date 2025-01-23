dc0d4fa4f1cb268c63782495edf1f8901f70b1f8
BONG-KI <qhdrl527@naver.com>
2025-01-23 11:26:00 +0900

    Title: K번 최댓값으로 이동하기, Time: 74ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250123/K번 최댓값으로 이동하기/move-to-max-k-times.py
+++ 250123/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -1,34 +1,25 @@
 n, k = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 r, c = map(int, input().split())
-r,c = r-1,c-1
+
 # 이동 방향 (상, 하, 좌, 우)
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 def dfs(x, y):
-    stack = [(x, y)]  # DFS 스택
-    visited = [[False] * n for _ in range(n)]  # 방문 여부
-    visited[x][y] = True
+    curscore = grid[x][y]
     maxscore = 0
     candidates = []
 
-    # DFS 탐색
-    while stack:
-        cx, cy = stack.pop()
-        curscore = grid[cx][cy]
-
-        for d in range(4):  # 4방향 탐색
-            nx, ny = cx + dx[d], cy + dy[d]
-            if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
-                if grid[nx][ny] < curscore:  # 현재 위치의 스코어보다 작은 경우만 이동 가능
-                    visited[nx][ny] = True
-                    stack.append((nx, ny))
-                    if grid[nx][ny] > maxscore:  # 가장 큰 스코어 업데이트
-                        maxscore = grid[nx][ny]
-                        candidates = [(nx, ny)]
-                    elif grid[nx][ny] == maxscore:  # 최대값이 동일하면 후보 추가
-                        candidates.append((nx, ny))
+    for d in range(4):  # 4방향 탐색
+        nx, ny = x + dx[d], y + dy[d]
+        if 0 <= nx < n and 0 <= ny < n:
+            if grid[nx][ny] < curscore:  # 현재 위치의 스코어보다 낮은 경우만 이동 가능
+                if grid[nx][ny] > maxscore:  # 가장 큰 스코어 업데이트
+                    maxscore = grid[nx][ny]
+                    candidates = [(nx, ny)]
+                elif grid[nx][ny] == maxscore:  # 최대값이 동일하면 후보 추가
+                    candidates.append((nx, ny))
     
     # 후보에서 행 우선, 열 우선 정렬하여 가장 작은 좌표 반환
     if candidates:
@@ -39,7 +30,7 @@ def dfs(x, y):
 
 # k번 이동
 for _ in range(k):
-    r, c = dfs(r, c)
+    r, c = dfs(r - 1, c - 1)
 
 # 최종 위치 출력
 print(r + 1, c + 1)
