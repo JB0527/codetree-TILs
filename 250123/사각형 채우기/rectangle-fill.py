2624c60dea63169b6ee9d5b19cfc190e3f3c7ab9
BONG-KI <qhdrl527@naver.com>
2025-01-23 14:56:34 +0900

    Title: 사각형 채우기, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250123/사각형 채우기/rectangle-fill.py
@@ -0,0 +1,14 @@
+n = int(input())
+dp = [0 for _ in range(n+1)]
+# # Write your code here!
+dp[0] = 1
+dp[1] = 1
+# n = 1 일때, 1
+# n= 2 일대, 2
+# n = 3 일대 3
+# n =4일대 , 2 3 5
+
+for i in range(1,n):
+    dp[i+1] = dp[i]+dp[i-1]
+
+print(dp[n])
