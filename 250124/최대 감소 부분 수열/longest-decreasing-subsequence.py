94b5eb32b09b23b9f3aaeed8d7fdf2d645b8a579
BONG-KI <qhdrl527@naver.com>
2025-01-24 10:54:21 +0900

    Title: 최대 감소 부분 수열, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250124/최대 감소 부분 수열/longest-decreasing-subsequence.py
+++ 250124/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -1,17 +1,12 @@
 n = int(input())
 m = list(map(int, input().split()))
-dp = [[0] for _ in range(n)]
+dp = [1] * n  # 각 위치에서의 감소 수열 최대 길이
 
-# Write your code here!
-# 최대의 방법 다 찾기 브루트포스 vs arr을 뒤집어서 하나씩 올라가기..
-#맨끝은 수열을 못만듦
-dp[0][0] =m[0]
-for i in range(n):
-    curscore= m[i]
-    for j in range(i+1,n):
-        #print(j)
-        if curscore > m[j]:
-            curscore = m[j]
-            dp[i].append(m[j])
-        else: continue
-print(len(max(dp)))
+# DP 계산
+for i in range(1, n):
+    for j in range(i):
+        if m[j] > m[i]:  # 감소 조건
+            dp[i] = max(dp[i], dp[j] + 1)
+
+# 결과 출력
+print((dp))
