3387c403c5d16d6ea46d5c5fe3b5f95cde5f4881
BONG-KI <qhdrl527@naver.com>
2025-02-03 10:11:00 +0900

    Title: 최대 점프 횟수, Time: 57ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250203/최대 점프 횟수/maximum-number-of-jumps.py
+++ 250203/최대 점프 횟수/maximum-number-of-jumps.py
@@ -3,14 +3,14 @@ arr = list(map(int, input().split()))
 dp = [0 for _ in range(n)]
 
 # Write your code here!
-dp[0] = 0
-jump = 0
+dp[0] = 1
 for idx,value in enumerate(arr):
     if value >= 1:
         for j in range(1,value+1):
-            if idx+j <n:
-                dp[idx+j] = max(dp[idx+j],dp[idx] + 1)
-        if idx < n-1:
-            if max(arr[idx+1:idx+j+1]) == 0:
-                break
-print(max(dp))
+            if idx+j <n and idx< n-1:
+                if max(arr[idx+1:idx+j+1]) != 0:
+                    dp[idx+j] = max(dp[idx+j],dp[idx] + 1)
+
+print(max(dp)-1)
+
+
