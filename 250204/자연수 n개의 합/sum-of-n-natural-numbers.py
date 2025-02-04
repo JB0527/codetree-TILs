2b9b97945148d7d4fccee0664a5bb9ee5f250ea1
BONG-KI <qhdrl527@naver.com>
2025-02-04 13:34:08 +0900

    Title: 자연수 n개의 합, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250204/자연수 n개의 합/sum-of-n-natural-numbers.py
+++ 250204/자연수 n개의 합/sum-of-n-natural-numbers.py
@@ -2,14 +2,13 @@ import math
 s = int(input())
 # Write your code here!
 left = 1 
-right = int(math.sqrt(s*2))
-min_num = s+1
+right = s
+max_num = 0
 while left <= right:
     mid = (left+right)//2
-    if mid * (mid+1)//2 > s:
-        right = mid - 1
-        min_num = min(min_num,mid)
-    else:
+    if mid * (mid+1)//2 <= s:
         left = mid +1
-
-print(mid-1)
+        max_num = max(max_num,mid)
+    else:
+        right = mid - 1
+print((left+right)//2)
