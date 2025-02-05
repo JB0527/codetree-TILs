7b4056c1a8cb2abaff16c1d12a85ecb74fbf2d7f
BONG-KI <qhdrl527@naver.com>
2025-02-05 16:41:57 +0900

    Title: 트리의 지름, Time: 575ms, Memory: 39MB, Status: Passed - CodeTree

--- 250205/트리의 지름/diameter-of-tree.py
+++ 250205/트리의 지름/diameter-of-tree.py
@@ -1,4 +1,53 @@
 import sys
+from collections import deque
+
+sys.setrecursionlimit(100000)
+
+n = int(input())
+edges = [[] for _ in range(n + 1)]
+
+# 그래프 입력 받기
+for _ in range(n - 1):
+    x, y, score = map(int, input().split())
+    edges[x].append((y, score))
+    edges[y].append((x, score))
+
+# BFS 함수
+def bfs(start):
+    visited = [False] * (n + 1)
+    total = [0] * (n + 1)
+    
+    queue = deque([start])
+    visited[start] = True
+    far_vertex = start
+    far_score = 0
+
+    while queue:
+        x = queue.popleft()
+        for y, score in edges[x]:
+            if not visited[y]:
+                visited[y] = True
+                total[y] = total[x] + score
+                queue.append(y)
+                
+                # 가장 먼 정점 갱신
+                if total[y] > far_score:
+                    far_score = total[y]
+                    far_vertex = y
+
+    return far_vertex, far_score
+
+# 1번 정점으로부터 가장 먼 정점 찾기
+f, _ = bfs(1)
+
+# f로부터 가장 먼 정점까지의 거리 찾기
+_, scores = bfs(f)
+
+# 결과 출력
+print(scores)
+
+"""
+import sys
 sys.setrecursionlimit(100000)
 from collections import deque
 n = int(input())
@@ -49,3 +98,4 @@ f,_ = find_largest_vertex(1)
 
 _,scores = find_largest_vertex(f)
 print(scores)
+"""
