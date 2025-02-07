c81669319e74abb5bc17b8828d4de3699daa8a18
BONG-KI <qhdrl527@naver.com>
2025-02-07 09:37:14 +0900

    Title: 연속한 K개의 숫자, Time: 78ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250207/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 250207/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -1,18 +1,18 @@
 N, K, B = map(int, input().split())
-missing = [int(input()) for _ in range(B)]
-
-# Write your code here!
+arr  = [0]*(N+1)
 prefix_sum = [0]*(N+1)
 
+# Write your code here!
+for i in range(B):
+    x = int(input())
+    arr[x]= 1
+# 지워야하는 곳에 1을 넣고 업데이트를 하는게 맞음 ㅇㅇ    
+prefix_sum[0] = 0
 for i in range(1,N+1):
-    if i not in missing:
-        prefix_sum[i] += 1
-        if prefix_sum[i-1] != 0:
-            prefix_sum[i] += prefix_sum[i-1]
-# print(prefix_sum)
-max_value = 0
-for i in range(N+1):
-    for j in range(i+1,N+1):
-        max_value = max(prefix_sum[j]+prefix_sum[i],max_value)
-        # print(max_value)
-print(K-max_value)
+    prefix_sum[i] = prefix_sum[i-1] + arr[i]
+
+#합찾기.. 연속하는 양만 해야하므로 k를 빼줌 그러면 추가해줘야하는 값만 나옴
+min_value = 0
+for i in range(1,N-k+2):
+    min_value = min(prefix_sum[i+k-1]-prefix_sum[i],min_value)
+print(min_value)
