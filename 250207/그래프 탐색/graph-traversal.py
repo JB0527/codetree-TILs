b9c04df517f8ee18509ac57260e6367efa5546b0
BONG-KI <qhdrl527@naver.com>
2025-02-07 09:57:32 +0900

    Title: 그래프 탐색, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250207/그래프 탐색/graph-traversal.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+arr = [tuple(map(int, input().split())) for _ in range(m)]
+edges = [[] for _ in range(n+1)]
+visited = [False] *(n+1)
+
+
+# Write your code here!
+for x,y in arr:
+    edges[x].append(y)
+    edges[y].append(x)
+print(edges)
+ans = []
+def traversal(x):
+    global ans
+    for y in edges[x]:
+        if not visited[y]:
+            visited[y] = True
+            traversal(y)
+            ans.append(1)
+
+traversal(1)
+print(len(ans)-1)
