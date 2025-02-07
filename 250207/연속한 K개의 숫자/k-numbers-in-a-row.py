742d43ad30b34c35f9db36e0e45535058d98a874
BONG-KI <qhdrl527@naver.com>
2025-02-07 09:38:07 +0900

    Title: 연속한 K개의 숫자, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250207/연속한 K개의 숫자/k-numbers-in-a-row.py
+++ 250207/연속한 K개의 숫자/k-numbers-in-a-row.py
@@ -1,4 +1,4 @@
-N, K, B = map(int, input().split())
+N, k, B = map(int, input().split())
 arr  = [0]*(N+1)
 prefix_sum = [0]*(N+1)
 
@@ -12,7 +12,7 @@ for i in range(1,N+1):
     prefix_sum[i] = prefix_sum[i-1] + arr[i]
 
 #합찾기.. 연속하는 양만 해야하므로 k를 빼줌 그러면 추가해줘야하는 값만 나옴
-min_value = 0
+min_value = 100002
 for i in range(1,N-k+2):
     min_value = min(prefix_sum[i+k-1]-prefix_sum[i],min_value)
 print(min_value)
