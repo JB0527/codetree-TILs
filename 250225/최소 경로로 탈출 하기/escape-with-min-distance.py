bb2875aeacc436585845eb7393e4e33f58c1b176
BONG-KI <qhdrl527@naver.com>
2025-02-25 13:56:35 +0900

    Title: 최소 경로로 탈출 하기, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250225/최소 경로로 탈출 하기/escape-with-min-distance.py
+++ 250225/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -3,37 +3,33 @@ from collections import deque
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-visited = [[False]*m for _ in range(n)]
-# Write your code here!
-step = [[0 for _ in range(m)] for _ in range(n)]
-
+visited = [[False] * m for _ in range(n)]
+step = [[0] * m for _ in range(n)]
 
 ans = 10002
-def bfs(x,y):
+dxs = [0, 1, 0, -1]
+dys = [1, 0, -1, 0]
+
+def bfs(x, y):
     global ans
     q = deque()
-    q.append((x,y))
+    q.append((x, y))
     visited[x][y] = True
+
     while q:
-        ax,ay = q.popleft()
-        for dx,dy in zip(dxs,dys):
-            nx = ax+dx
-            ny = ay+dy
-            if 0<=nx<n and 0<=ny<m :
-                if arr[nx][ny]==1 and visited[nx][ny] == False:
+        ax, ay = q.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = ax + dx, ay + dy
+            if 0 <= nx < n and 0 <= ny < m:
+                if arr[nx][ny] == 1 and not visited[nx][ny]:
                     visited[nx][ny] = True
-                    step[nx][ny] += (step[ax][ay]+1)
-                    if nx == m-1 and ny == n-1:
-                        ans = step[m-1][n-1]
-                    q.append((nx,ny))
+                    step[nx][ny] = step[ax][ay] + 1
+                    if nx == n - 1 and ny == m - 1:
+                        ans = step[n - 1][m - 1] + 1
+                    q.append((nx, ny))
 
-dxs = [0,1,0,-1]
-dys = [1,0,-1,0]
-
-
-bfs(0,0)
+bfs(0, 0)
 
 if ans == 10002:
     ans = -1
 print(ans)
-
