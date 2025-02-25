287d6383504fe90e81fae9bfa6a5e2f2f85dc6a0
BONG-KI <qhdrl527@naver.com>
2025-02-25 13:44:36 +0900

    Title: 최소 경로로 탈출 하기, Time: 71ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 250225/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+visited = [[False]*m for _ in range(n)]
+# Write your code here!
+step = [[0 for _ in range(m)] for _ in range(n)]
+
+
+ans = 10002
+def bfs(x,y):
+    global ans
+    q = deque()
+    q.append((x,y))
+    visited[x][y] = True
+    while q:
+        ax,ay = q.popleft()
+        for dx,dy in zip(dxs,dys):
+            nx = ax+dx
+            ny = ay+dy
+            if 0<=nx<m and 0<=ny<n and arr[nx][ny]==1 and visited[nx][ny] == False:
+                visited[nx][ny] = True
+                step[nx][ny] += (step[ax][ay]+1)
+                ans = step[m-1][n-1]
+                if nx == m-1 and ny == n-1:
+                    print(ans)
+                    exit(0)
+                q.append((nx,ny))
+
+dxs = [0,1,0,-1]
+dys = [1,0,-1,0]
+
+
+bfs(0,0)
+
+if ans == 10002:
+    print(-1)
+
