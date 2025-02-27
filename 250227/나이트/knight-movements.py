0c3663b0d8026cc7ad85e32345f4b0a6b7d57a15
BONG-KI <qhdrl527@naver.com>
2025-02-27 17:55:45 +0900

    Title: 나이트, Time: 52ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250227/나이트/knight-movements.py
@@ -0,0 +1,32 @@
+from collections import deque
+n = int(input())
+r1, c1, r2, c2 = map(int, input().split())
+
+visited = [[False for _ in range(n)] for _ in range(n)]
+# Please write your code here.
+
+dxs = [-1,-1,-2,-2,1,1,2,2]
+dys = [-2,2,-1,1,-2,2,-1,1]
+
+def bfs(i,j,step):
+    q= deque()
+    q.append((i,j,step))
+    while q:
+        i,j,step = q.popleft()
+        if i == r2 and j ==c2:
+            return step
+        for dy,dx in zip(dys,dxs):
+            ny = dy+j
+            nx = dx+i
+            if 0<=nx<n and 0<=ny<n :
+                if visited[nx][ny] == False:
+                    q.append((nx,ny,step+1))
+                    visited[nx][ny] = True
+    return step
+visited[r1-1][c1-1] = True
+ans = bfs(r1-1,c1-1,0)
+
+if not visited[r2-1][c2-1]:
+    print(-1)
+else:
+    print(ans)
